707bf474b7a78b3143a5b44520a04771a27d3f1e
Anastasia <nekoluchka91@gmail.com>
2022-10-17 11:48:10 +0400

    Add HW to Seminar2

--- HW_sem2_copy.py
+++ HW_sem2_copy.py
@@ -16,22 +16,14 @@
 # Задача 2. Напишите программу, которая принимает на вход 
 # число N и выдает набор произведений чисел от 1 до N.
 # N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-# т.е. 1 2 3 4 
-# не понимаю как написать программу (())
-# n = int(input('Введите N: '))
-# def select(f, col):
-#     return [f(x) for x in col]
-# def where (f, col):
-#     return [x for x in col if f(x)]
 
-# data = '1 2 3 4'.split()
-# res = select(int, data)
-# res = where(lambda x: x, res)
-# res = select(lambda x: x, factorial(x))
-# print(res)
-
-
-
+# N = int(input('N: '))
+# f = 1
+# for i in range(N):
+#     i = i + 1
+#     f = i * f
+    
+#     print(f, end=", ")
 
 # Задача 3. Задайте список из n чисел 
 # последовательности (1 + 1 / n) ** n 
@@ -40,7 +32,43 @@
 # 2 -> 4.25
 # 3 -> 6.62037037037037
 
-
 # n = int(input('Введите число N: ')) 
 # list = [(1  + 1 / n) ** n for n in range (1, n  +  1)]        
 # print ((sum(list)))
+
+
+# Задача 4. Задайте список из N элементов, 
+# заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+# path = 'file.txt'
+# data = open (path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# n = int(input('N: '))
+# print(*range(-n, n + 1), sep = ', ')
+# Не понимаю дальше как (
+
+
+# Задача 5. Реализуйте алгоритм перемешивания списка.
+
+# from random import Random, randint
+# N = int(input('Введите число ')) # ввожу количество цифр в списке
+# numbers = [] # сначала список пустой
+# for i in range(N):
+#     numbers.append(randint(-N, N+1)) # задаю отрезок от N до -N
+# print(numbers) # вывожу список на экран
+
+# def new(numbers): # создаем новый список
+#     list = numbers[:]
+#     numbers_length = len(list) # длина списка
+#     for i in range(numbers_length):
+#         index = randint(0, numbers_length - 1) # инд списка от [0] до посл
+#         temp = list[i] # вводим вр переменную, чтобы перезаписывать
+#         list[i] = list[index] 
+#         list[index] = temp
+#     return list # возвращаем список
+# print(new(numbers)) # печатаем на экран нов список
